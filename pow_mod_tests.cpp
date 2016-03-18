66a5d1055afda0b808e157aaf71afb664b405a59
mirraz <mirraz@example.com>
2016-03-18 12:45:17 +0500

    change test_pow_mod

--- pow_mod_tests.cpp
+++ pow_mod_tests.cpp
@@ -7,11 +7,17 @@
 void test_pow_mod() {
 	typedef PowMod<uint_fast32_t, ((uint_fast32_t)1)<<31, uint_fast64_t> pow_mod_type;
 	pow_mod_type::num_type mod, base, exp, pow;
-	mod = 4294967291;
-	base = 2;
-	exp = (mod-1)/2;
-	pow = pow_mod_type::pow_mod(mod, base, exp);
-	printf("%u^%u mod %u = %u\n", base, exp, mod, pow);
+	for (mod = 2; mod<256; ++mod) {
+		for (base = 0; base<256; ++base) {
+			for (exp = 0; exp < 16; ++exp) {
+				pow = pow_mod_type::pow_mod(mod, base, exp);
+				printf(
+					"mod = %" PRIuFAST32 " base = %" PRIuFAST32 " exp = %" PRIuFAST32 " pow = %" PRIuFAST32 "\n",
+					mod, base, exp, pow
+				);
+			}
+		}
+	}
 }
 
 void tests_suite() {
