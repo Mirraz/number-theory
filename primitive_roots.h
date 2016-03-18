4b12c321949be8e752dd707e58c3686e5e4e561e
mirraz <mirraz@example.com>
2016-03-18 16:16:23 +0500

    add primitive_roots

---
+++ primitive_roots.h
@@ -0,0 +1,59 @@
+#ifndef PRIMITIVE_ROOTS_H
+#define PRIMITIVE_ROOTS_H
+
+#include <assert.h>
+#include <stdint.h>
+#include "factorize.h"
+#include "pow_mod.h"
+
+template <typename NUM_TYPE, uint_fast8_t MAX_PRIME_FACTORS_COUNT, NUM_TYPE NUM_TYPE_MAX_MASK, typename OPERATION_TYPE>
+class PrimitiveRoots {
+public:
+	typedef NUM_TYPE num_type;
+private:
+	typedef GetPrimeFactors<num_type> get_prime_factors_type;
+	typedef PowMod<num_type, NUM_TYPE_MAX_MASK, OPERATION_TYPE> pow_mod_type;
+public:
+	typedef typename get_prime_factors_type::prime_factors_count_type prime_factors_count_type;
+	typedef typename get_prime_factors_type::primes_array_type primes_array_type;
+	typedef typename get_prime_factors_type::factorizer_type factorizer_type;
+	
+private:
+	num_type modulo;
+	num_type exps[MAX_PRIME_FACTORS_COUNT];
+	prime_factors_count_type exps_count;
+	
+public:
+	// modulo must be prime
+	PrimitiveRoots(primes_array_type b_primes_array, num_type b_modulo) : modulo(b_modulo) {
+		assert(modulo >= 2);
+		if (modulo <= 3) {
+			exps_count = 0;
+			return;
+		}
+		num_type modulo_1 = modulo - 1;
+		
+		num_type prime_factors[MAX_PRIME_FACTORS_COUNT];
+		get_prime_factors_type get_prime_factors(b_primes_array);
+		exps_count = get_prime_factors.get_prime_factors(modulo_1, prime_factors, MAX_PRIME_FACTORS_COUNT);
+		
+		for (prime_factors_count_type i=0; i<exps_count; ++i) {
+			assert(modulo_1 > prime_factors[i]);
+			assert(modulo_1 % prime_factors[i] == 0);
+			exps[exps_count-i-1] = modulo_1 / prime_factors[i];
+		}
+	}
+	
+	// gcd(modulo, root) == 1
+	bool is_primitive_root(num_type root) {
+		assert(pow_mod_type::pow_mod(modulo, root, modulo-1) == 1);
+		for (prime_factors_count_type i=0; i<exps_count; ++i) {
+			num_type pow = pow_mod_type::pow_mod(modulo, root, exps[i]);
+			if (pow == 1) return false;
+		}
+		return true;
+	}
+};
+
+#endif/*PRIMITIVE_ROOTS_H*/
+
---
+++ primitive_roots_tests.cpp
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "primitive_roots.h"
+
+void test_is_primitive_root() {
+	// primorial(9) <= 2^32-1 < primorial(10)
+	typedef PrimitiveRoots<uint_fast32_t, 9, ((uint_fast32_t)1)<<31, uint_fast64_t> prrs_type;
+	
+	// pi(2^16) = 6542
+	prrs_type::num_type primes[6542];
+	size_t primes_count = prrs_type::factorizer_type::fill_primes(
+		primes,
+		sizeof(primes) / sizeof(primes[0]),
+		(prrs_type::num_type)UINT16_MAX + 1
+	);
+	assert(primes_count == sizeof(primes) / sizeof(primes[0]));
+	prrs_type::primes_array_type primes_array(primes, primes_count);
+	
+	for (size_t idx=0; idx<sizeof(primes) / sizeof(primes[0]); ++idx) {
+		prrs_type::num_type modulo = primes[idx];
+		prrs_type primitive_roots(primes_array, modulo);
+		prrs_type::num_type min_root = 0;
+		for (prrs_type::num_type i=1; i<modulo; ++i) {
+			bool is_primitive_root = primitive_roots.is_primitive_root(i);
+			if (is_primitive_root) {min_root = i; break;}
+		}
+		prrs_type::num_type max_root = 0;
+		for (prrs_type::num_type i=modulo-1; i>=1; --i) {
+			bool is_primitive_root = primitive_roots.is_primitive_root(i);
+			if (is_primitive_root) {max_root = i; break;}
+		}
+		assert(min_root != 0);
+		assert(max_root != 0);
+		assert((modulo-1) % 4 != 0 || modulo-max_root == min_root);
+		printf("%" PRIuFAST64 "\t %" PRIuFAST64 "\n", modulo, min_root);
+		printf("%" PRIuFAST64 "\t-%" PRIuFAST64 "\n", modulo, modulo-max_root);
+		printf("\n");
+	}
+}
+
+void tests_suite() {
+	test_is_primitive_root();
+}
+
+int main() {
+	tests_suite();
+	return 0;
+}
